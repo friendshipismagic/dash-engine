906af66affd323abd1a32130b362b8880fdf24f4
Clement Decoodt <clement@decoodt.eu>
2016-11-25 15:33:49 +0100

    First basic demo with freeGLUT
    Using freeGLUT to generate procedurally a triangle.

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -24,6 +24,15 @@ void reshape(int width, int height) {
 // Display callback.
 // called when possible to update the display
 void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-0.5,-0.5,0.0);
+	glVertex3f(0.5,0.0,0.0);
+	glVertex3f(0.0,0.5,0.0);
+	glEnd();
+
+	glutSwapBuffers();
 }
 
 // Keyboard callback.
@@ -71,5 +80,7 @@ int main(int argc, char **argv) {
 	glutIdleFunc(idle);
 
 	// enter GLUT event processing cycle
+	glutMainLoop();
 
+	return 0;
 }
