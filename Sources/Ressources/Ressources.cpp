7879fabecfd8420472601f18a4bfa4fe742ce204
Clement Decoodt <clement@decoodt.eu>
2016-11-26 15:52:11 +0100

    WIP: Ressource main manager
    This ressource manager takes the whole ressources and acts as an API
    between the modules to handle ressources

---
+++ Sources/Ressources/Ressources.cpp
@@ -0,0 +1,16 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <clement@decoodt.eu> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Clement Decoodt
+ * ----------------------------------------------------------------------------
+ */
+
+#include "Ressources.h"
+
+namespace Ressources {
+	void Manager::init() {
+		// TODO: Load all the ressources and manage them :)
+	}
+}
