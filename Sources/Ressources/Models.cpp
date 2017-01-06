f3f3dd8945381717337b39946dd9f485d5eb894f
Clément Decoodt <clement@decoodt.eu>
2017-01-06 13:46:32 +0100

    WIP: Adding scene/models importer
    using assimp for that. It should be in thirdparty folder

---
+++ Sources/Ressources/Models.cpp
@@ -0,0 +1,20 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <clement@decoodt.eu> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Clement Decoodt
+ * ----------------------------------------------------------------------------
+ */
+
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+
+#include "Ressources/Ressources.h"
+
+#include "Ressources/Models.h"
+
+namespace Ressources {
+	void Models::init(std::shared_ptr<Ressources::Manager>, std::string fn) {
+	}
+}
---
+++ Sources/Ressources/Models.h
@@ -0,0 +1,32 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <clement@decoodt.eu> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Clement Decoodt
+ * ----------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "Ressources/Ressources.h"
+
+namespace Ressources {
+	class Models {
+		public:
+			// Default constructor, should not do anything
+			Models() {};
+			// Default destructor, should not do anything
+			~Models() {};
+
+			// Initializer, taking a model filename and ressource manager to get
+			// the Models folder
+			void init(std::shared_ptr<Ressources::Manager>, std::string fn);
+
+			// Get the scene object (everything is seen as a scene)
+			std::shared_ptr<aiScene> getScene();
+	};
+}
