383b501eb109d2fe54da5c122963b574c14bf251
Clément Decoodt <clement@decoodt.eu>
2017-02-14 18:13:48 +0100

    Don't need this texture management anymore

--- Sources/Ressources/Mesh.cpp
+++ Sources/Ressources/Mesh.cpp
@@ -65,32 +65,7 @@ namespace Ressources {
 		// Set Shader Program to use
 		glUseProgram(shaderProgram);
 
-        // Bind appropriate textures
-        GLuint diffuseNr = 1;
-        GLuint specularNr = 1;
-        for(GLuint i=0; i<this->textures.size(); i++)
-        {
-			// Active proper texture
-            glActiveTexture(GL_TEXTURE0 + i);
-            // Retrieve texture number (the N in diffuse_textureN)
-			// Using same conventions as learopengl.com
-			// So also using their code here :)
-			std::stringstream ss;
-			std::string number;
-            std::string name = this->textures[i].type;
-            if(name == "texture_diffuse")
-                ss << diffuseNr++; // Transfer GLuint to stream
-            else if(name == "texture_specular")
-                ss << specularNr++; // Transfer GLuint to stream
-            number = ss.str(); 
-            // Now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shaderProgram,
-						(name + number).c_str()), i);
-            // And finally bind the texture
-            glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
-        }
-        
-        // Also set each mesh's shininess property to a default value
+        // Set each mesh's shininess property to a default value
         glUniform1f(glGetUniformLocation(shaderProgram, "material.shininess"),
 				16.0f);
 
@@ -98,12 +73,5 @@ namespace Ressources {
         glBindVertexArray(this->VAO);
         glDrawElements(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
-
-        // Always good practice to set everything back to defaults once configured.
-        for (GLuint i = 0; i < this->textures.size(); i++)
-        {
-            glActiveTexture(GL_TEXTURE0 + i);
-            glBindTexture(GL_TEXTURE_2D, 0);
-        }
 	}
 }
