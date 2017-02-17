e163d4ffe70ce3597f35e087077a1147a2b169c4
Clément Decoodt <clement@decoodt.eu>
2017-02-17 20:00:32 +0100

    Private UBO updater definition and UBO struct definition

--- Sources/Renderer/Lights.h
+++ Sources/Renderer/Lights.h
@@ -21,6 +21,28 @@ namespace Renderer {
 			void toggleLight(int id);
 
 		private:
+			void update_UBO();
+
+			// Default structure of the UBO
+			// This structure is the same as defined in OpenGL Shader
+			// Language Specifications in V 1.30 and newer
+			struct gl_LightSourceParameters {
+				glm::vec4 ambient; // Acli
+				glm::vec4 diffuse; // Dcli
+				glm::vec4 specular; // Scli
+				glm::vec4 position; // Ppli
+				glm::vec4 halfVector; // Derived: Hi
+				glm::vec3 spotDirection; // Sdli
+				float spotExponent; // Srli
+				float spotCutoff; // Crli
+				// (range: [0.0,90.0], 180.0)
+				float spotCosCutoff; // Derived: cos(Crli)
+				// (range: [1.0,0.0],-1.0)
+				float constantAttenuation; // K0
+				float linearAttenuation; // K1
+				float quadraticAttenuation;// K2
+			};
+
 			// UBO Id
 			GLuint UBO;
 	};
